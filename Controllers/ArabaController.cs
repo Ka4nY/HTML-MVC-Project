707324892974b7913d7b8f7358aaf949351627de
Ka4nY <kaanyuceyurt2008@gmail.com>
2024-02-29 18:56:51 +0300

    first commit

---
+++ Controllers/ArabaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore8MVC.Models;
+
+
+
+namespace NetCore8MVC.Controllers
+{
+    public class ArabaController : Controller
+    {
+      
+        public ArabaController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+          
+            TCar araba = new TCar(); 
+            araba.Marka = "Ford"; 
+            araba.Model = "Mustang";
+            araba.ModelYil = 2008;
+            araba.Renk = "Kırmızı";
+
+            return View(TCar); 
+        }
+        
+        public IActionResult Ekle()
+        {
+            return View();
+        }z<
+    }
+}
